ad4d22b6786d6d5876ed843d6699801cd5f2825f
lywen <lywen.nagaline@gmail.com>
2024-02-01 11:12:06 +0100

    intégration mongoose et auth

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,35 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tpbqupg.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+// on utilise express.json pour transformer le corps de la requête en objet JS utilisable. Il est possible d'utiliser bodyparser mais il est inclus dans express depuis la version 4.16.0
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // * = tout le monde peut accéder à l'API
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  // on donne l'autorisation d'utiliser certains headers
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  // on donne l'autorisation d'utiliser certaines méthodes
+  next();
+});
+
 //app.use("/api/offer", offerRoutes);
 //app.use("/api/auth", userRoutes);
 
