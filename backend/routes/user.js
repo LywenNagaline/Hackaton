a87dab3d29130f11bebb723a68c052a658486c0d
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2024-02-02 13:37:35 +0100

    Ajout du bouton de déconnexion

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
-const auth = require("../middlewares/auth");
+/* const auth = require("../middlewares/auth"); */
 
-router.post("/signup", auth, userCtrl.signup);
-router.post("/login", auth, userCtrl.login);
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 //Les routes doivent d'abord passer par le système d'authentification avant de pouvoir se login ou s'inscire
 
 module.exports = router;
