acc3e51beb8529bed55daf291e68172f6c3000e5
lywen <lywen.nagaline@gmail.com>
2024-02-02 09:43:19 +0100

    ajout des tests en cours

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/signup", auth, userCtrl.signup);
+router.post("/login", auth, userCtrl.login);
 // ne pas oublier de rajouter auth dans les routes qui doivent être protégées
 
 module.exports = router;
