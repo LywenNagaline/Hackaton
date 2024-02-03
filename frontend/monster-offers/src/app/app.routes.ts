57b76bfeb02b1d6a37e7c55a12d7b15740570629
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2024-02-03 13:27:35 +0100

    Ajout d'une page test accessible à localhost:4200/test

--- frontend/monster-offers/src/app/app.routes.ts
+++ frontend/monster-offers/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import {LoginComponent} from "./login/login.component";
 import {JobOfferComponent} from "./job-offer/job-offer.component";
 import {HOME_PATH, LOGIN_PATH} from "./core/constants/app.constant";
 import {AuthGuard} from "./core/guards/authorization.guard";
+import { PageTestComponent } from './page-test/page-test.component';
 
 export const routes: Routes = [
   {path: LOGIN_PATH, component: LoginComponent},
   {path: "", redirectTo: LOGIN_PATH, pathMatch: "full"},
   {path: HOME_PATH, component: JobOfferComponent, canActivate : [AuthGuard]},
+  {path: "test", component: PageTestComponent},
 ];
